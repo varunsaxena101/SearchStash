4ea5eb17871d568e76fcdd042836110d8dcc5985
Varun S <varun.saxena101@gmail.com>
2019-12-26 11:47:13 -0600

    updated description in manifest and commented out content.js

--- content.js
+++ content.js
@@ -3,24 +3,24 @@
  *  It will eventually be used to highlight the selected text on a webpage.
  */
 
-chrome.contextMenus.onClicked.addListener(function(clickInfo) {
-    if (clickInfo.menuItemId == 'save' && clickInfo.selectionText) {
-        // alert(clickInfo.selectionText);
-        highlight();
-    }
-});
+// chrome.contextMenus.onClicked.addListener(function(clickInfo) {
+//     if (clickInfo.menuItemId == 'save' && clickInfo.selectionText) {
+//         // alert(clickInfo.selectionText);
+//         highlight();
+//     }
+// });
 
-function highlight() {
-	const selection = document.getSelection();
-	if (selection.toString() !== '') {
-		const range = selection.getRangeAt(0);
-		const span = document.createElement('span');
-		span.className = 'highlighted-text';
-		span.style.backgroundColor = 'yellow';
-		span.style.cursor = 'pointer';
-		span.innerHTML = range.toString();
-		range.deleteContents();
-		range.insertNode(span);
-		// addCSS();
-	}
-}
+// function highlight() {
+// 	const selection = document.getSelection();
+// 	if (selection.toString() !== '') {
+// 		const range = selection.getRangeAt(0);
+// 		const span = document.createElement('span');
+// 		span.className = 'highlighted-text';
+// 		span.style.backgroundColor = 'yellow';
+// 		span.style.cursor = 'pointer';
+// 		span.innerHTML = range.toString();
+// 		range.deleteContents();
+// 		range.insertNode(span);
+// 		// addCSS();
+// 	}
+// }
